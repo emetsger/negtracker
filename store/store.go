53127ae8cff6b7ea607162e2ab9b9361d7cb0075
Elliot Metsger <emetsger@jhu.edu>
2020-09-07 02:38:12 -0400

    Formalized error values being returned from store.Api - StorageError struct - Sentinel values for common errors - Does not wrap underlying Mongo errors so clients do not depend on them (use the sentinel values instead) - Custom Is(error) function to examine possible sentinels.

--- store/store.go
+++ store/store.go
@@ -2,6 +2,11 @@
 // persistence layer.
 package store
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Presents an API for durably storing business objects.
 type Api interface {
 
@@ -25,3 +30,39 @@ const (
 	EnvDbName          = "DB_NAME"
 	EnvDbNegCollection = "DB_NEG_COLLECTION"
 )
+
+type StorageError struct {
+	sentinel   error
+	msg, cause string
+}
+
+func SentinelErr(sentinel error, msg, cause string) StorageError {
+	if sentinel == nil {
+		panic("store: error creating error, sentinel value required")
+	}
+	return StorageError{sentinel, msg, cause}
+}
+
+func GenericErr(msg, cause string) error {
+	return StorageError{GeneralErr, msg, cause}
+}
+
+func (e StorageError) Error() string {
+	if len(e.msg) > 0 {
+		if len(e.cause) > 0 {
+			return fmt.Sprintf("%s, %s: %s", e.sentinel.Error(), e.msg, e.cause)
+		} else {
+			return fmt.Sprintf("%s, %s", e.sentinel.Error(), e.msg)
+		}
+	}
+
+	return e.sentinel.Error()
+}
+
+var GeneralErr = errors.New("store: error occurred interacting with the storage layer")
+var DuplicateKeyErr = errors.New("store: attempt to insert a duplicate key")
+var DecodingErr = errors.New("store: error decoding object")
+
+func (e StorageError) Is(target error) bool {
+	return target == GeneralErr || target == DuplicateKeyErr || target == DecodingErr
+}
