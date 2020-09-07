21df6ab78ef7e68756e73db5ca080bd92ae92b7a
Elliot Metsger <emetsger@jhu.edu>
2020-09-06 22:14:36 -0400

    Refactors the Entity struct into a WebResource interface.
    The Entity struct is clumsy for our purposes:
    - Structs do not inherit from each other in an object-oriented sense.  A struct that embeds the Entity struct is *not* of type `Entity`.  We wish to perform type assertions, and embedded structs are not a path forward for that.  Interfaces are.
    - "Entity" is a bad name, it implies (to my mind) some sort of relationship with the persistence layer, and this is not true.  The persistence layer will store and retrieve anything that uses "id" as the key for storing the object's key.  What we really mean is that this is a web-based resource, and these fields on "Entity" are being used in the HTTP layer to address web concerns like caching and emit proper HTTP headers
    
    Therefore, the use of embedded structs are removed and replaced with an interface named WebResource.
    
    Model structs that require treatment as web resources will need to implement the WebResource interface, which at this juncture has some ugliness with it, because the common fields are not captured in an embedded struct.

--- model/model.go
+++ model/model.go
@@ -1,18 +1,79 @@
 package model
 
 import (
+	"github.com/emetsger/negtracker/etag"
 	"github.com/emetsger/negtracker/store"
+	"strings"
 	"time"
 )
 
-type Entity struct {
-	ID      string
-	Created time.Time
-	Updated time.Time
+// Encapsulates a business object that is exposed as a web resource
+//
+// Every business object should implement this interface, so that a type assertion with this interface may be used by
+// functions accepting the `interface{}` type.
+type WebResource interface {
+	// Obtain the business identifier of the resource, which is guaranteed to be immutable once the resource has been
+	// durably persisted.
+	GetId() string
+	// Obtain the UTC time the resource was created.
+	GetCreated() time.Time
+	// Obtain the UTC time the state of the resource was last updated.
+	GetUpdated() time.Time
+	// Obtain the HTTP ETag of the resource, suitable for performing validation of resource state
+	GetEtag() Etag
+	// Set the business identifier of the resource, which is guaranteed to be immutable once the resource has been
+	// durably persisted
+	SetId(id string)
+	// Set the UTC time the resource was created.
+	SetCreated(t time.Time)
+	// Set the UTC time the state of the resource was last updated
+	SetUpdated(t time.Time)
+}
+
+// Encapsulates an ETag and whether it is a weak or strong validator.  The zero value of an ETag is the same as that
+// of the type `string`: a zero-length empty string.
+type Etag string
+
+// Returns true if the ETag is a strong validator.  Panics if the ETag is an empty string.
+func (e Etag) strong() bool {
+	if string(e) == "" {
+		panic("model: etag is empty")
+	}
+	return !strings.HasPrefix(string(e), "W/")
+}
+
+func (e *Neg) GetId() string {
+	return e.Id
+}
+
+func (e *Neg) GetCreated() time.Time {
+	return e.Created
+}
+
+func (e *Neg) GetUpdated() time.Time {
+	return e.Updated
+}
+
+func (e *Neg) SetId(id string) {
+	e.Id = id
+}
+
+func (e *Neg) SetCreated(t time.Time) {
+	e.Created = t
+}
+
+func (e *Neg) SetUpdated(t time.Time) {
+	e.Updated = t
+}
+
+func (e *Neg) GetEtag() Etag {
+	return Etag(etag.NewEncoder().AddString(e.Id).AddTime(e.Created).AddTime(e.Updated).Encode(true))
 }
 
 type Neg struct {
-	Entity
+	Id          string
+	Created     time.Time
+	Updated     time.Time
 	Film        string
 	EI          int
 	Developer   string
