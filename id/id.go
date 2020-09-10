6c6c9b042483483a4ea09de31bd6d14105b87ec4
Elliot Metsger <emetsger@jhu.edu>
2020-09-09 23:28:53 -0400

    WIP: Refactoring to fix Test_BusinessIdLockFail

--- id/id.go
+++ id/id.go
@@ -17,6 +17,7 @@ type Minter interface {
 	Mint(interface{}) string
 }
 
+// Mints a UUID
 func Mint() string {
 	return uuid.New().String()
 }
@@ -32,26 +33,40 @@ type BusinessId struct {
 }
 
 // Obtain the lock of this BusinessId prior to entering a critical path
-func (bid BusinessId) Lock() {
+func (bid *BusinessId) Lock() {
+	// Contact the lockTable for the current *sync.Mutex and lock it.
+	bid.l = getLock(bid.idTypeTuple)
 	bid.l.Lock()
 }
 
 // Release the lock of this BusinessId after exiting a critical path
-func (bid BusinessId) Unlock() {
+func (bid *BusinessId) Unlock() {
+	// Unlock the *sync.Mutex and Contact the lockTable via a chan and return it.
 	bid.l.Unlock()
+
+	// We don't nil this out, primarily so that unit tests can compare the value of the lock between two different
+	// BusinessId structs.
+	//bid.l = nil
 }
 
-// Return the (identifier, type) tuple for this BusinessId
+// Obtain a business id for the given identifier string and type.
+func GetId(id string, t interface{}) BusinessId {
+	tuple := idTypeTuple{id, typeAsString(t)}
+	bid := BusinessId{idTypeTuple: tuple}
+	return bid
+}
+
+// Return the (identifier, type) tuple for this BusinessId as a string
 func (bid BusinessId) String() string {
-	return fmt.Sprintf("Id: %s Type: %s", bid.idTypeTuple.Id, bid.idTypeTuple.Type)
+	return fmt.Sprintf("Id: %s Type: %s", bid.idTypeTuple.bid, bid.idTypeTuple.bidType)
 }
 
 // Tuple representing a unique business identifier
 type idTypeTuple struct {
 	// The string identifier
-	Id string
+	bid string
 	// The type being identified (as a string)
-	Type string
+	bidType string
 }
 
 // Maintains a least recently used cache and table mapping idTypeTuple to sync.Mutex
@@ -60,46 +75,62 @@ type lockTable struct {
 	mu sync.Mutex
 	// The capacity of the table and lru
 	cap int
-	// Maps idTypeTuple to list elements
+	// Maps idTypeTuple to list elements (whose Value() returns *sync.Mutex)
 	table map[idTypeTuple]*list.Element
 	// lru of BusinessId instances
 	lru *list.List // elements are BusinessId instances
 }
 
-// global lock table, with a capacity for 100 business ids
+// Global lock table, with a capacity for 100 business ids.  When the capacity is exceeded, the table is scanned and
+// the least recently used business id is removed.  Not super efficient when the table gets full.
 var locks = lockTable{sync.Mutex{}, 100, make(map[idTypeTuple]*list.Element, 100), &list.List{}}
-
-// Obtain a business id for the given identifier string and type.  The returned BusinessId
-func GetId(id string, t interface{}) BusinessId {
-	locks.mu.Lock()
-	defer func() { locks.mu.Unlock() }()
-
-	// check to see if the business id exists in the table, if so, push it to the head of the list and return it
-	key := idTypeTuple{id, typeAsString(t)}
-	if elem, ok := locks.table[key]; ok {
-		locks.lru.MoveToFront(elem)
-		return elem.Value.(BusinessId)
-	}
-
-	// if the business id does not exist in the table:
-	// 	1. create the id
-	//	3. push the id onto the head of the list
-	//	4. put the resulting element in the table
-	//  5. if the list has exceeded capacity, remove the tail and the entry in the table
-	//	6. return business id
-
-	bid := BusinessId{key, &sync.Mutex{}}
-	elem := locks.lru.PushFront(bid)
-	locks.table[key] = elem
-
-	if locks.lru.Len() > locks.cap {
-		removed := locks.lru.Remove(locks.lru.Back()).(BusinessId)
-		delete(locks.table, removed.idTypeTuple)
-	}
-
-	return bid
-}
 
 func typeAsString(t interface{}) string {
 	return fmt.Sprintf("%T", t)
+}
+
+// Obtain a lock for an (id, type) tuple.  Multiple requests for the same (id, type) tuple should return the same
+// *sync.Mutex.
+func getLock(tuple idTypeTuple) *sync.Mutex {
+	// Guards access to the map and list
+	locks.mu.Lock()
+	defer func() { locks.mu.Unlock() }()
+
+	// check to see if the lock for the type tuple exists in the table,
+	// if so, push it to the head of the list and return it
+	if elem, ok := locks.table[tuple]; ok {
+		locks.lru.MoveToFront(elem)
+		return elem.Value.(*sync.Mutex)
+	}
+
+	// if the type tuple does not exist in the table:
+	// 	1. create a new *sync.Mutex for the type tuple
+	//	2. push the *sync.Mutex onto the head of the list
+	//	3. put the resulting element in the table for the tuple
+	//  4a. if the list has exceeded capacity, 4b. remove the list tail and its corresponding entry in the table
+	//	5. return the lock for the id
+
+	l := &sync.Mutex{}             // 1
+	elem := locks.lru.PushFront(l) // 2
+	locks.table[tuple] = elem      // 3
+
+	// TODO: this is quite sub-optimal.  Once the table fills to capacity, it is scanned each time for values to
+	//  evict.  It would be nice to avoid this scan.
+	if locks.lru.Len() > locks.cap { // 4a
+		eValue := locks.lru.Remove(locks.lru.Back()) // 4b
+		eValue.(*sync.Mutex).Lock()                  // FIXME: locked entries can't be removed so this may block,
+		//  preventing other ids from being generated.  This will
+		//  happen when > cap business ids have been Locked(), yet
+		//  to be Unlock()ed
+		defer func() { eValue.(*sync.Mutex).Unlock() }()
+
+		// scan map values to remove the lru entry
+		for k, v := range locks.table {
+			if v.Value == eValue {
+				delete(locks.table, k) // 4b
+			}
+		}
+	}
+
+	return l
 }
--- id/id_test.go
+++ id/id_test.go
@@ -6,12 +6,11 @@ import (
 	"github.com/stretchr/testify/require"
 	"math"
 	"math/rand"
-	"sync"
 	"testing"
 	"time"
 )
 
-// validate initial state and that one entry is added to the table and lru each time a different business id is
+// validate initial state and that one entry is added to the table and lru each time a lock for a unique business id is
 // requested.
 func Test_GetBusinessId(t *testing.T) {
 	// there should be no entries in the table or list
@@ -24,22 +23,37 @@ func Test_GetBusinessId(t *testing.T) {
 	bid := GetId(uuid, bidType)
 	require.NotNil(t, bid)
 
-	// there should be one element in the table and list, and they should contain the newly created business id
-	expectedBid := BusinessId{idTypeTuple{uuid, typeAsString(bidType)}, &sync.Mutex{}}
+	// Lock the business id, which should create one element in the lock table and list for the idTypeTuple
+	bid.Lock()
+
+	expectedLock := bid.l
 	require.Equal(t, 1, len(locks.table))
 	require.Equal(t, 1, locks.lru.Len())
-	require.Equal(t, expectedBid, locks.lru.Front().Value)
-	require.Equal(t, expectedBid, locks.table[idTypeTuple{uuid, typeAsString(bidType)}].Value)
+	require.Same(t, expectedLock, locks.lru.Front().Value)
+	require.Same(t, expectedLock, locks.table[idTypeTuple{uuid, typeAsString(bidType)}].Value)
 
-	// Get the same business id, the table and list size shouldn't change
-	bid = GetId(uuid, bidType)
-	require.NotNil(t, bid)
+	// Unlock the business id later, which shouldn't change the size of the table or its values.
+	bid.Unlock()
+
+	require.Equal(t, 1, len(locks.table))
+	require.Equal(t, 1, locks.lru.Len())
+	require.Same(t, expectedLock, locks.lru.Front().Value)
+	require.Same(t, expectedLock, locks.table[idTypeTuple{uuid, typeAsString(bidType)}].Value)
+
+	// Get the same business id but assign to a different variable.  The table and list size shouldn't change.
+	bid2 := GetId(uuid, bidType)
 	require.Equal(t, 1, len(locks.table))
 	require.Equal(t, 1, locks.lru.Len())
 
-	// Get another, and the table and list should increment by one
-	bid = GetId("foo", bidType)
-	require.NotNil(t, bid)
+	// Lock bid2 (which populates BusinessId.l).  Both bid and bid2 should share the same lock.
+	bid2.Lock()
+	require.Same(t, bid.l, bid2.l)
+	defer func() { bid2.Unlock() }()
+
+	// Get a different business id.  Obtain its lock.  The table and list size should increment by 1.
+	bid3 := GetId("foo", bidType)
+	bid3.Lock()
+	defer func() { bid3.Unlock() }()
 	require.Equal(t, 2, len(locks.table))
 	require.Equal(t, 2, locks.lru.Len())
 }
@@ -48,7 +62,12 @@ func Test_GetBusinessId(t *testing.T) {
 func Test_GetBusinessIdCap(t *testing.T) {
 	var i int
 	for i = len(locks.table); i <= locks.cap*2; i++ {
-		_ = GetId(Mint(), model.Neg{})
+		// Generate the id
+		id := GetId(Mint(), model.Neg{})
+		// Add an entry in the locking table
+		id.Lock()
+		// Immediately release the lock so the lru table scan can complete
+		id.Unlock()
 	}
 
 	require.Equal(t, locks.cap, len(locks.table))
@@ -92,10 +111,22 @@ func Test_BusinessIdLockSame(t *testing.T) {
 	id := "moo"
 	n := model.Neg{}
 	bid1 := GetId(id, n)
+	require.Nil(t, bid1.l) // idTypeTuple.l is initially nil, only initialized when Lock() is called.
+	bid1.Lock()
+	bid1.Unlock() // Because Lock() initializes  idTypeTuple.l, it must be called.  Because bid1 and
+	// bid2 are using the same lock, Unlock() must be called otherwise bid2.Lock() will
+	// hang.
 	bid2 := GetId(id, n)
+	require.Nil(t, bid2.l)
+	bid2.Lock()
+	bid2.Unlock()
+	require.NotNil(t, bid2.l)
+
 	require.Same(t, bid1.l, bid2.l)
 
 	bid3 := GetId("foo", n)
+	bid3.Lock()
+	bid3.Unlock()
 	require.NotSame(t, bid1.l, bid3.l)
 }
 
@@ -125,25 +156,33 @@ func Test_BusinessIdLock(t *testing.T) {
 }
 
 func Test_BusinessIdLockFail(t *testing.T) {
-	// 1. obtain a business id
-	// 2. generate exactly enough business ids to expire the id obtained by 1
+	// 1. obtain a business id, then lock it to create an entry in the lock table, then unlock it.
+	// 2. generate exactly enough business ids to expire the id obtained by 1; verify that bid1 has been expired from the table
 	// 3. obtain a business id using the same tuple as 1
-	// 4. verify they do not share a lock.
+	// 4. attempt to lock the bid from 1, verify they share a lock.
 
 	// 1
 	id := "moo"
 	n := model.Neg{}
 	bid1 := GetId(id, n)
+	bid1.Lock()
+	bid1.Unlock() // insure an entry is created in the table
 
 	// 2
 	for i := 0; i < locks.cap; i++ {
-		_ = GetId(Mint(), n)
+		j := GetId(Mint(), n)
+		j.Lock()
+		j.Unlock() // insure an entry is created in the table
 	}
-	require.Nil(t, locks.table[idTypeTuple{id, typeAsString(n)}])
+	require.Nil(t, locks.table[idTypeTuple{id, typeAsString(n)}]) // the idTypeTuple for bid1 has been expired.
 
 	// 3
 	bid2 := GetId(id, n)
+	bid2.Lock()
+	bid2.Unlock() // initialize the idType.l
 
 	// 4
+	bid1.Lock()
+	bid1.Unlock() // should get the latest lock from the table, the same lock used by bid2.
 	require.Same(t, bid1.l, bid2.l)
 }
